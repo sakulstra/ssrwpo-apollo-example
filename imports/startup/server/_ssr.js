a42365244d6055d0ccc50ff0876fdc96cd7471fc
Corentin Bilotta <corentin@mighdy.com>
2017-06-30 15:52:24 +0200

    Fix SSR

--- imports/startup/server/_ssr.js
+++ imports/startup/server/_ssr.js
@@ -1,18 +1,23 @@
 import React from 'react'
-import {createRouter, logger} from 'meteor/ssrwpo:ssr'
-import {ApolloProvider} from 'react-apollo'
+import { createRouter, logger } from 'meteor/ssrwpo:ssr'
+import { ApolloProvider } from 'react-apollo'
 import 'isomorphic-fetch'
-import {RootRouter} from '/imports/env/router'
-import {client} from '/imports/env/apollo'
+import { RootRouter } from '/imports/env/router'
+import { client } from '/imports/env/apollo'
+import PropTypes from 'prop-types';
 
-const MainApp = (props, context) => {
-	console.log(context.store) //is empty
+const MainApp = (props, { store }) => {
+	console.log(store) //is empty
 	return (
-		<ApolloProvider client={client}>
+		<ApolloProvider store={store} client={client}>
 			<RootRouter/>
 		</ApolloProvider>
 	)
 }
+
+MainApp.contextTypes = {
+	store: PropTypes.object.isRequired,
+};
 
 createRouter(MainApp)
 logger.info('started ssr')
